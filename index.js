5387cbd457ea334cb94071354a095b0188ceac36
Iqmii <bilousdavid1910@gmail.com>
2025-02-28 18:46:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+
+         }
+        });
+    }
+let options = {threshold: [0.4]};
+let observer = new IntersectionObserver (onEntry, options);
+let elements = document.querySelectorAll('.text-block2-animation');
+for (let elm of elements) {
+    observer.observe (elm);
+}
