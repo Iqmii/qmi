a5c3a9442c65ca08cd1523b16bc8638ca3226ede
Iqmii <bilousdavid1910@gmail.com>
2025-03-01 11:27:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ let elements = document.querySelectorAll('.text-block2-animation');
 for (let elm of elements) {
     observer.observe (elm);
 }
+
+
+
+
+
+
+function showNotfication() {
+    const notification = document.getElementById('notification');
+    notification.classList.add('show');
+    console.log('show');
+
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+        console.log('showTime')
+    }, 3000);
+}
+
+
+if (sessionStorage.getItem('registrationSuccess') === 'true') {
+    showNotfication();
+    sessionStorage.removeItem('registrationSuccess');
+    console.log('38 index')
+}
+
